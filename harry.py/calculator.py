ceba297361b7f99193e0cd2f00cc0bbc1888d5c1
Abhishekkp421 <abhishek6387669323@gmail.com>
2026-05-28 18:16:38 +0530

    Delete harry.py directory

--- harry.py/calculator.py
+++
@@ -1,8 +0,0 @@
-a=50
-b=3
-
-print("the value of",a,"+",3,"is:",a+b)
-print("the value of",a,"-",3,"is:",a-b)
-print("the value of",a,"*",3,"is:",a*b)
-print("the value of",a,"/",3,"is:",a/b)
-print("the value of",a,"%",3,"is:",a%b) 
